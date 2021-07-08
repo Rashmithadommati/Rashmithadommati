ae22f8c3d88b8d46b9a996be871a6583552c00f4
Rashmithadommati <83545913+Rashmithadommati@users.noreply.github.com>
2021-07-08 08:39:12 +0530

    Add files via upload

---
+++ EnrollmentDaoImpl.java
@@ -0,0 +1,25 @@
+package com.citi.membership.enrollment.dao;
+
+import com.citi.membership.enrollment.model.EnrollmentDaoResponse;
+import com.citi.membership.enrollment.model.EnrollmentRequest;
+
+public class EnrollmentDaoImpl implements EnrollmentDao {
+	public EnrollmentDaoResponse  createEnroll(EnrollmentRequest enrollmentRequest) {
+		//1.get the request from service 
+		//2. prepare the request for database i.e., prepare the database queries
+		//3.call db and get db response i.e., resultset
+		//4. prepare the dao response 
+		
+		EnrollmentDaoResponse enrollmentDaoResponse = new EnrollmentDaoResponse();
+		
+		
+		
+		
+		
+		return enrollmentDaoResponse;
+		
+	}
+	
+	
+
+}
